691cf20d6f9b78e02c7066e04bc75c75cb23371b
Jonas B <jonas.bacelis@gmail.com>
2015-09-16 22:29:54 +0200

    hashtaging, time stamp, reproducibility, flag codes for changes made

--- MoBa_cleaning_MaternalHeightWeight.R
+++ MoBa_cleaning_MaternalHeightWeight.R
@@ -1,15 +1,22 @@
 
-#  cleaning height and weight variables for MoBa projects
-# by Jonas B.
+# a script for cleaning height and weight variables in MoBa projects
+# by Jonas Bacelis
 # 2015 09 16
 
-# phenotypes (height and weight)
+
+# load the phenotypes (height and weight)
+#  1) note! this script expects first column name to be Preg_ID
+#  2) note! this script expects other columns to be "AA85" "AA86" "AA87"
 q1=read.csv("~/Biostuff/MOBA_1555_CLEANING/PDB1555_Skjema1_v8.csv",h=T,sep=";",stringsAsFactors=F)
 colnames(q1)[1]="PREG_ID"
 head(q1); dim(q1)
 
+# save a primal state for describing all changes in the end
+q1_backup = q1
 
-# maternal ID vs PregID data
+
+# load maternal ID and PregID data
+# note! this script expects first column to be Preg_ID and second column - MaternalID
 svi = read.csv("~/Biostuff/MOBA_1555_CLEANING/PDB1555_SV_INFO_v8.csv",h=T,";",stringsAsFactors=F)
 colnames(svi) = c("PREG_ID","M_ID")
 head(svi); dim(svi)
@@ -20,9 +27,10 @@ sum(duplicated(q1$PREG_ID))
 sum(duplicated(svi$PREG_ID))
 
 
-# define the function of plotting many values
+# define the function of plotting large quantities of values values
 library(hexbin)
 fun_plot = function(X,Y,LOG,XLAB,YLAB) {
+        # LOG = 100 works best
         h=hexbin(Y~X); x=h@xcm; y=h@ycm; s=h@count
         plot(x,y,pch=1,cex=log(s,LOG),xlab=XLAB,ylab=YLAB,
              xlim=range(X,na.rm=T),ylim=range(Y,na.rm=T))
@@ -254,17 +262,18 @@ fun_bmi = function(weight,height,thrL,thrR) {
 ########################
 ########################
 
-### first round of cleaning: hand-writing and OCR problems
-# play with constants
+### first round of cleaning: CURATION of hand-writing problems and OCR problems
+# play with constants, and when you are satisfied - proceed further
 w1 = fun_weightCuration(weight=q1$AA85,low=30,upp=200,thr=3,xlim=NA)
 w2 = fun_weightCuration(weight=q1$AA86,low=30,upp=200,thr=3,xlim=NA)
 h1 = fun_heightCuration(q1$AA87,130,210,3)
 
+# summary of changes
 table(w1$flag)
 table(w2$flag)
 table(h1$flag)
 
-# assign new values
+# assign new (corrected) values
 q1$AA85 = w1$weight
 q1$AA86 = w2$weight
 q1$AA87 = h1$height
@@ -273,24 +282,41 @@ q1$AA87 = h1$height
 ########################
 ########################
 
-# second round of cleaning (various cleaning steps)
-w1w2_1 = fun_weightVSweight(q1$AA85,q1$AA86,30,-30); table(w1w2$flag)
-w1h1_1 = fun_weightVSheight(q1$AA85,q1$AA87,4,5); table(w1h1$flag)
-w2h1_1 = fun_weightVSheight(q1$AA86,q1$AA87,4,5); table(w2h1$flag)
-bm1_1 = fun_bmi(q1$AA85,q1$AA87,thrL=4,thrR=5); table(bm1$flag)
-bm2_1 = fun_bmi(q1$AA86,q1$AA87,thrL=4,thrR=5); table(bm2$flag)        
+# second round of cleaning:  various deletions
+# first, play with constants, view summaries, and when happy - proceed
+w1w2_1 = fun_weightVSweight(q1$AA85,q1$AA86,30,-30); table(w1w2_1$flag)
+w1h1_1 = fun_weightVSheight(q1$AA85,q1$AA87,4,5); table(w1h1_1$flag)
+w2h1_1 = fun_weightVSheight(q1$AA86,q1$AA87,4,5); table(w2h1_1$flag)
+bm1_1 = fun_bmi(q1$AA85,q1$AA87,thrL=4,thrR=5); table(bm1_1$flag)
+bm2_1 = fun_bmi(q1$AA86,q1$AA87,thrL=4,thrR=5); table(bm2_1$flag)        
 
-# delete appropriate values
-q1$AA85[which(w1w2$flag=="deleted")]=NA
-q1$AA85[which(w1h1$flag=="deleted")]=NA
-q1$AA85[which(bm1$flag=="deleted")]=NA
-q1$AA86[which(w1w2$flag=="deleted")]=NA
-q1$AA86[which(w2h1$flag=="deleted")]=NA
-q1$AA86[which(bm2$flag=="deleted")]=NA
-q1$AA87[which(w1h1$flag=="deleted")]=NA
-q1$AA87[which(w2h1$flag=="deleted")]=NA
-q1$AA87[which(bm1$flag=="deleted")]=NA
-q1$AA87[which(bm2$flag=="deleted")]=NA
+# preview selected deletions in one plot:
+fun_plot(q1$AA87,q1$AA85,100,"height","weight")
+points(AA85~AA87,data = q1[which(bm1_1$flag=="deleted"),],pch=19,col="green")
+points(AA85~AA87,data = q1[which(bm2_1$flag=="deleted"),],pch=19,col="grey")
+points(AA85~AA87,data = q1[which(w1w2_1$flag=="deleted"),],pch=19,col="blue")
+points(AA85~AA87,data = q1[which(w1h1_1$flag=="deleted"),],pch=19,col="red")
+points(AA85~AA87,data = q1[which(w2h1_1$flag=="deleted"),],pch=19,col="turquoise")
+
+fun_plot(q1$AA87,q1$AA86,100,"height","weight (preg)")
+points(AA86~AA87,data = q1[which(bm1_1$flag=="deleted"),],pch=19,col="green",cex=1.2)
+points(AA86~AA87,data = q1[which(bm2_1$flag=="deleted"),],pch=3,col="black")
+points(AA86~AA87,data = q1[which(w1w2_1$flag=="deleted"),],pch=19,col="blue",cex=0.8)
+points(AA86~AA87,data = q1[which(w2h1_1$flag=="deleted"),],pch=19,col="red",cex = 0.6)
+points(AA86~AA87,data = q1[which(w1h1_1$flag=="deleted"),],pch=1,col="black",cex=2)
+
+
+# delete selected ("untrustworthy") values
+q1$AA85[which(w1w2_1$flag=="deleted")]=NA
+q1$AA85[which(w1h1_1$flag=="deleted")]=NA
+q1$AA85[which(bm1_1$flag=="deleted")]=NA
+q1$AA86[which(w1w2_1$flag=="deleted")]=NA
+q1$AA86[which(w2h1_1$flag=="deleted")]=NA
+q1$AA86[which(bm2_1$flag=="deleted")]=NA
+q1$AA87[which(w1h1_1$flag=="deleted")]=NA
+q1$AA87[which(w2h1_1$flag=="deleted")]=NA
+q1$AA87[which(bm1_1$flag=="deleted")]=NA
+q1$AA87[which(bm2_1$flag=="deleted")]=NA
 
 
 ########################
@@ -303,10 +329,10 @@ q1$AA87[which(bm2$flag=="deleted")]=NA
 mids = svi[unique(which(duplicated(svi$M_ID))),"M_ID"]
 rixs = which(svi$M_ID %in% mids)
 pids = unique(svi$PREG_ID[rixs])
-Q1 = q1[which(q1$PREG_ID %in% pids),]
+Q1 = q1[which(q1$PREG_ID %in% pids),] # smaller dataset to increase the speed
 pids1 = pids[which(pids %in% Q1$PREG_ID)]
-svi = svi[which(svi$PREG_ID %in% pids1),]
-col_name1="AA87"; col_name2="AA85"; col_name3="AA86"
+svi = svi[which(svi$PREG_ID %in% pids1),] # reduction of dataset to increase the speed
+col_name1="AA87"; col_name2="AA85"; col_name3="AA86"   # colnames!!! ***   
 rez = NULL
 for (i in 1:nrow(Q1)) {
         # note that the larger preg id means the later pregnancy (time increases with preg id)
@@ -327,13 +353,11 @@ for (i in 1:nrow(Q1)) {
         rez = rbind(rez,tmp)
         rm(ix,mid,ixs,prgids,rixs,tmp,parit)
 }
-row.names(rez) = Q1$PREG_ID
-head(rez)
+head(rez); dim(rez)
 
 
 
-
-# special functions that finds max and min value in REMAINING pregnancies of the same mom
+# function that find max and min values in the REMAINING pregnancies of the same mother
 fun_max = function(x) {
         x = x[-c(x[11],11)] # eliminate column representing pregnancy in question
         ifelse(sum(!is.na(x))>0,max(x,na.rm=T),NA) # report
@@ -354,39 +378,39 @@ w2mxs = as.numeric(apply(rez[,c(21:30,31)],1,fun_max))
 w2mns = as.numeric(apply(rez[,c(21:30,31)],1,fun_min))
 
 
-
 # delete only if a value differs from extreme values..
 # ..in other pregnancies of the same mother by more than X units
 h1tst = w1tst = w2tst = NULL
-for (i in 1:nrow(q1)) {
+for (i in 1:nrow(q1)) {  # takes around 1 minute..
         print(paste(i,nrow(q1),sep=" / "))
         ix = which(prgid==q1$PREG_ID[i])[1] # [1] = due to duplicates
         if(length(ix)>0) {
-                h1tst = c(h1tst, (q1$AA87[i] >= h1mxs[ix] + 5)|(q1$AA87[i] <= h1mns[ix] - 5))
-                w1tst = c(w1tst, (q1$AA85[i] >= w1mxs[ix] + 20)|(q1$AA85[i] <= w1mns[ix] - 20))
-                w2tst = c(w2tst, (q1$AA86[i] >= w2mxs[ix] + 20)|(q1$AA86[i] <= w2mns[ix] - 20))
+                h1tst = c(h1tst, (q1$AA87[i] >= h1mxs[ix] + 5)|(q1$AA87[i] <= h1mns[ix] - 5))  # arbitrary! ***
+                w1tst = c(w1tst, (q1$AA85[i] >= w1mxs[ix] + 20)|(q1$AA85[i] <= w1mns[ix] - 20)) # arbitrary! ***
+                w2tst = c(w2tst, (q1$AA86[i] >= w2mxs[ix] + 20)|(q1$AA86[i] <= w2mns[ix] - 20)) # arbitrary! ***
         } else {
                 h1tst = c(h1tst,NA)
                 w1tst = c(w1tst,NA)
                 w2tst = c(w2tst,NA)
         }
-}
+} # end of cycling
 
 
+# inspect the flags
 table(h1tst)
 table(w1tst)
 table(w2tst)
 
 
-# third cleaning (repeated various cleaning steps)
+# third round of cleaning:  various deletions
+# first, play with constants, view summaries, and when happy - proceed
 w1w2_2 = fun_weightVSweight(q1$AA85,q1$AA86,30,-30); table(w1w2_2$flag)
 w1h1_2 = fun_weightVSheight(q1$AA85,q1$AA87,4,5); table(w1h1_2$flag)
 w2h1_2 = fun_weightVSheight(q1$AA86,q1$AA87,4,5); table(w2h1_2$flag)
 bm1_2 = fun_bmi(q1$AA85,q1$AA87,thrL=4,thrR=5); table(bm1_2$flag)
 bm2_2 = fun_bmi(q1$AA86,q1$AA87,thrL=4,thrR=5); table(bm2_2$flag)        
 
-# preview deletions:
-
+# preview selected deletions in one plot:
 fun_plot(q1$AA87,q1$AA85,100,"height","weight")
 points(AA85~AA87,data = q1[which(bm1_2$flag=="deleted"),],pch=19,col="green")
 points(AA85~AA87,data = q1[which(bm2_2$flag=="deleted"),],pch=19,col="grey")
@@ -395,7 +419,6 @@ points(AA85~AA87,data = q1[which(w1h1_2$flag=="deleted"),],pch=19,col="red")
 points(AA85~AA87,data = q1[which(w2h1_2$flag=="deleted"),],pch=19,col="turquoise")
 points(AA85~AA87,data = q1[which(h1tst),],pch=19,col="black",cex = 0.7)
 points(AA85~AA87,data = q1[which(w1tst),],pch=19,col="red",cex = 0.7)
-
 
 fun_plot(q1$AA87,q1$AA86,100,"height","weight (preg)")
 points(AA86~AA87,data = q1[which(bm1_2$flag=="deleted"),],pch=19,col="green",cex=1.2)
@@ -407,9 +430,7 @@ points(AA86~AA87,data = q1[which(h1tst),],pch=19,col="black",cex = 0.7)
 points(AA86~AA87,data = q1[which(w2tst),],pch=19,col="red",cex = 0.7)
 
 
-###  final cleaning
-
-           
+###  create a table of all flags 
 out = data.frame(bm1_1=(bm1_1$flag=="deleted"),bm2_1=(bm2_1$flag=="deleted"),
                  w1w2_1=(w1w2_1$flag=="deleted"),w1h1_1=(w1h1_1$flag=="deleted"),
                  w2h1_1=(w2h1_1$flag=="deleted"),
@@ -417,38 +438,76 @@ out = data.frame(bm1_1=(bm1_1$flag=="deleted"),bm2_1=(bm2_1$flag=="deleted"),
                  w1w2_2=(w1w2_2$flag=="deleted"),w1h1_2=(w1h1_2$flag=="deleted"),
                  w2h1_2=(w2h1_2$flag=="deleted"),
                  h1 = h1tst, w1 = w1tst, w2 = w2tst, stringsAsFactors=F)
-head(out)
-
+head(out); dim(out)
 
 # vectors that decide what should be deleted
 fun_sum = function(x) sum(x,na.rm=T)
-h1t = apply(out[,c(1,2,4,5, 6,7,9,10, 11)],1,fun_sum)
-w1t = apply(out[,c(1,3,4,   6,8,9,    12)],1,fun_sum)
-w2t = apply(out[,c(2,3,5,   7,8,10,   13)],1,fun_sum)
+h1t = apply(out[,c(1,2,4,5, 6,7,9,10, 11)],1,fun_sum) # for height
+w1t = apply(out[,c(1,3,4,   6,8,9,    12)],1,fun_sum) # for prepreg weight
+w2t = apply(out[,c(2,3,5,   7,8,10,   13)],1,fun_sum) # for pregnancy weight
 table(h1t); table(w1t); table(w2t)
 
-sum(is.na(q1$AA85))
-sum(is.na(q1$AA86))
-sum(is.na(q1$AA87))
-
-# delete values
+###  delete what is selected for deletion
 q1$AA85[which(w1t>0)]=NA
 q1$AA86[which(w2t>0)]=NA
 q1$AA87[which(h1t>0)]=NA
 
-# missing
+# check how many are missing now
 sum(is.na(q1$AA85))
 sum(is.na(q1$AA86))
 sum(is.na(q1$AA87))
 
+# how many pregnancies have (and how many do not) a full set of data
 sum( (is.na(q1$AA85))|(is.na(q1$AA87)))
 sum( (!is.na(q1$AA85))&(!is.na(q1$AA87)))
 
 
+# create a dataframe of flags describing what happened with each Pregnancy
+flags = data.frame(PREG_ID=q1$PREG_ID,AA85=NA,AA86=NA,AA87=NA)
+for (col_name in c("AA85","AA86","AA87")) {
+        changes = as.numeric(q1[,col_name] != q1_backup[,col_name])
+        changes[is.na(changes)] = 0
+        deletions = (is.na(q1[,col_name]))&(!is.na(q1_backup[,col_name]))
+        # table(changes); table(deletions)
+        #since changes were done first and only then -  deletions :
+        changes[which(deletions)]=2
+        table(changes,useNA="a")
+        flags[,col_name] = changes
+        rm(changes,deletions)
+}
+
+# preview
+head(flags)
+table(flags$AA85)
+table(flags$AA86)
+table(flags$AA87)
 
 
+# rename columns so that they would not be confused with original data columns
+ex_names = colnames(flags)[grep("^AA",colnames(flags))]
+new_names = paste("fl",ex_names,sep="")
+colnames(flags)[grep("^AA",colnames(flags))] = new_names
+head(flags)
 
 
+# create the object that will be saved
+output_obj = data.frame(q1,flags[,-grep("PREG_ID",colnames(flags))])
+head(output_obj)
+
+####################  for reproducibility reasons:
+# get the date stamp
+date_stamp = paste(unlist(strsplit(substr(Sys.time(),1,10),"-")),collapse="")
+# get the time stamp
+time_stamp = paste(unlist(strsplit(substr(Sys.time(),12,19),":")),collapse="")
+# get the hash of the current state of the Git folder
+hash = system(paste("git log --pretty=format:'%h' -n 1"),intern=TRUE)
+# generate the file name suffix
+suffix = paste(date_stamp,time_stamp,hash,sep="_")
+
+
+file_dir = "~/Biostuff/MOBA_1555_CLEANING/"
+file_name = paste(file_dir,"MOBA_PDB1555_CLEANED_maternalHgh1Wgh1Wgh2_",suffix,".txt",sep="")
+write.table(output_obj,file_name,row.names=F,col.names=T,sep="\t",quote=F)
 
 
 
